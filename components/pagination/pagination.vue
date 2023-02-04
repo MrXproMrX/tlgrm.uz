27ef3aad0bd3aee76d7242c8db78745faf462148
MRXPRO200330020179Y <mrxpro2003@gmail.com>
2023-02-04 12:46:45 +0500

    sticker

--- components/pagination/pagination.vue
+++ components/pagination/pagination.vue
@@ -3,7 +3,13 @@
        <ul class="pagination__list">
 
          <li class="pagination__item" v-if="page>1">
-           <a class="pagination__link pagination__link--arrow pagination__link--disabled" aria-label="Предыдущая страница"  @click.prevent="paginate(page -1)">
+            <a class="pagination__link" href="#" aria-label="Следующая страница" @click.prevent="paginate(1)" >
+               <i class="fas fa-angle-double-left"></i>
+            </a>
+         </li>
+
+         <li class="pagination__item" v-if="page>1">
+           <a class="pagination__link" aria-label="Предыдущая страница"  @click.prevent="paginate(page -1)">
              <i class="fas fa-angle-left"></i>
            </a>
          </li>
@@ -15,9 +21,15 @@
          </li>
 
          <li class="pagination__item" v-if="page<pages">
-           <a class="pagination__link pagination__link--arrow" href="#" aria-label="Следующая страница" @click.prevent="paginate(page+1)" >
-             <i class="fas fa-angle-right"></i>
-           </a>
+            <a class="pagination__link" href="#" aria-label="Следующая страница" @click.prevent="paginate(page+1)" >
+               <i class="fas fa-angle-right"></i>
+            </a>
+         </li>
+
+         <li class="pagination__item" v-if="page<pages">
+            <a class="pagination__link" href="#" aria-label="Следующая страница" @click.prevent="paginate(pages)" >
+               <i class="fas fa-angle-double-right"></i>
+            </a>
          </li>
 
        </ul>
---
+++ components/sticker/stickerAnime.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="sticker__list">
+      <div class="sticker__item" v-for="(animes,i) in anime" :key="i">
+         <div class="sticker__img">
+            <img :src="animes.imgs" :alt="animes.linkText">
+         </div>
+
+         <a :href="animes.link" class="sticker__link" target="_blank">{{ animes.linkText }}</a>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   props:['anime']
+}
+</script>
